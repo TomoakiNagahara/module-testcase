f1f6f6fd935c5c6cefe1dcd745a15473515f710e
Tomoaki -imak5k- <tomoaki.nagahara+imac5k@gmail.com>
2017-12-08 12:52:27 +0900

    NEW: common.php

---
+++ common.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * module-testcase:/common.php
+ *
+ * @creation  2017-11-21
+ * @version   1.0
+ * @package   app-skeleton
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+Template::Run('../common.php');
+
+//	...
+App::Title('testcase');
