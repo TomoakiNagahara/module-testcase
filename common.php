32a842161cc91fe5bb1417652c1df62223ecf7d1
Tomoaki -imak5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-02-07 16:19:52 +0900

    New: common.php, menu.phtml

--- common.php
+++ common.php
@@ -13,3 +13,30 @@ Template::Run('../common.php');
 
 //	...
 App::Title('testcase');
+
+//	...
+if(!Env::isAdmin() ){
+	exit();
+}
+
+//	...
+if(!Env::Get('testcase')){
+	//	...
+	$config = [
+		'driver'   => 'mysql',
+		'port'     => '3306',
+		'host'     => 'localhost',
+		'user'     => 'testcase',
+		'password' => 'testcase',
+		'database' => 'testcase',
+		'charset'  => 'utf8',
+	];
+
+	//	...
+	Env::Set('testcase', $config);
+}
+?>
+<!-- Up -->
+<div>
+	<a href="../">UP</a>
+</div>
