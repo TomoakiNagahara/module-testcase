b680a6bdb13c44d04c6ad5501fc1c89dfb9c7a50
Tomoaki -imak5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-02-07 16:18:25 +0900

    New: unit/index.php, common.php, menu.phtml

---
+++ unit/common.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * module-testcase:/unit/common.php
+ *
+ * @creation  2018-01-09
+ * @version   1.0
+ * @package   module-testcase
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/../common.php');
---
+++ unit/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * module-testcase:/unit/index.php
+ *
+ * @creation  2018-01-23
+ * @version   1.0
+ * @package   module-testcase
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/common.php');
+?>
+<?php include(__DIR__.'/menu.phtml') ?>
